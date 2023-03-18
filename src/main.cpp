756442f7f3efc166dc24043019b6bd53f7f4a3dc
Tomas Prerovsky <cepsdev@hotmail.com>
2023-03-18 22:57:02 +0100

    Parsing states.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
-int main(int argc, char** argv){
+using namespace std;
+struct Logs{
 
+
+};
+
+int main(int argc, char** argv){
+    for(auto i = 1; i < argc; ++i)
+    {
+        ifstream is{argv[i]};
+        for(string s;getline(is,s);){
+            auto last_pos{0};
+            for(auto j{0}; j < s.length(); ++j){
+                if (s[j] != '+' && s[j] != '-') continue;
+                string state = s.substr(last_pos, j - last_pos + 1);
+                cout << state << endl;
+                for(last_pos = j+1;last_pos < s.length() && s[last_pos] == ' '; ++last_pos);
+            }
+        }
+    }
 }

fd7fa03c6cf5dba5fec3eea3058cc5bf457194e7
Tomas Prerovsky <cepsdev@hotmail.com>
2023-03-20 00:13:35 +0100

    Poor Man's Suffix Tree - couple of details missing.

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,113 @@
 #include <vector>
 
 using namespace std;
+
+
+
 struct Logs{
-
-
+    vector<int> v;
+    void push(int i){
+        v.push_back(i);
+    }
+    vector<int> const & data() const {
+        return v;
+    }
 };
 
+struct PoorMansSuffixTree{
+ 
+ struct Branch{
+    int seq_start;
+    int next_branch;
+ };
+ 
+ struct Character{
+    char ch;
+    int next;
+ };
+
+ struct Node{
+  Node() {
+    branch = Branch{0,0};
+  }
+  union {
+    Branch branch;
+    Character character;
+  };
+ };
+ 
+ vector<Node> tree;
+ int cur_it = 0;
+ int last_it = 0;
+ bool cur_it_branch{true};
+ bool last_it_branch{true};
+ 
+ PoorMansSuffixTree() {
+    tree.push_back(Node{});
+ };
+
+ void start(char ch){
+    cur_it = 0;
+    last_it = 0;
+    cur_it_branch = true;
+    last_it_branch = true;
+ }
+
+ Node& node() {
+    return tree[cur_it];
+ }
+ Node& node(int j) {
+    return tree[j];
+ }
+ 
+ Node& node_before() {
+    return tree[last_it];
+ }
+
+ void step(char ch){
+    //INVARIANT cur_t points to a Character
+    auto& n{node()};
+    if (n.character.next > 0 && node(n.character.next).character.ch == ch);
+     //cur_it =  
+ }
+
+ int code(){
+    return cur_it;
+ }
+};
+
+ostream& operator << (ostream& os, Logs const & logs){
+    for(auto i : logs.data())
+    {
+        if (!i) os << '\n' << '\n';
+        else os << i << ' ';
+    }
+    return os;
+}
+
 int main(int argc, char** argv){
+    PoorMansSuffixTree suffix_tree;
+    Logs logs;
+
     for(auto i = 1; i < argc; ++i)
     {
         ifstream is{argv[i]};
         for(string s;getline(is,s);){
+            if (s.length() < 2) continue;
             auto last_pos{0};
-            for(auto j{0}; j < s.length(); ++j){
+            suffix_tree.start(s[0]);
+            for(auto j{1}; j < s.length(); ++j){
+                suffix_tree.step(s[j]);
                 if (s[j] != '+' && s[j] != '-') continue;
                 string state = s.substr(last_pos, j - last_pos + 1);
-                cout << state << endl;
+                logs.push(suffix_tree.code());                
+                //cout << state << endl;
                 for(last_pos = j+1;last_pos < s.length() && s[last_pos] == ' '; ++last_pos);
+                if (last_pos < s.length()) suffix_tree.start(s[last_pos]);
             }
+            logs.push(0);
         }
     }
+    cout << logs << '\n';
+
 }
